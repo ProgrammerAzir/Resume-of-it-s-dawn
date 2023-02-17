60c9634bec99c72a0aa83915150947c462baa484
DESKTOP-7TKUD7T\天将明 <641408809@qq.com>
2023-02-17 20:03:16 +0800

    feat：调整全局样式

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,12 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
-
-import './assets/main.css'
+import pinia from '@/stores/pinia_main'
+import './assets/main.less'
 
 const app = createApp(App)
 
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,21 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server:{
+    port:6008,
+  },
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  css: {
+    // 预处理器配置项
+    preprocessorOptions: {
+      less: {
+        math: "always",
+      },
+    },
+  },
 })
